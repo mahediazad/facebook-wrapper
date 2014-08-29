65caf49558c593204e23071a96e17b7a7e9bf9d1
mahediazad <mahediazad@gmail.com>
2014-08-29 15:29:27 +0600

    Composer init and facebook class

---
+++ index.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by Md. Mahedi Azad
+ * User: Micro
+ * Date: 8/29/14
+ * Time: 3:16 PM
+ */
+require_once ("FacebookBootstrap.php");
+
+$facebook = new FacebookBootstrap();
+
+
+
+
+?>
